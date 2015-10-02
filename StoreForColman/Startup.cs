d384b099d9257022ccd1821a9a9a143a40330296
Gal Schlezinger <galstar@spitfire.co.il>
2015-10-02 01:58:08 -0400

    initial commit

---
+++ StoreForColman/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoreForColman.Startup))]
+namespace StoreForColman
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
